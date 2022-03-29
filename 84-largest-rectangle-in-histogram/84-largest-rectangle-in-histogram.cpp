f4527b324d1624d1640689ef601d69f789114646
Yash Mangroliya <77552168+YashMangroliya@users.noreply.github.com>
2022-03-29 17:50:49 +0530

    Time: 218 ms (46.65%), Space: 75.4 MB (36.55%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -27,48 +27,67 @@ public:
         
         // Optimized Solution
         
+//         int n=heights.size();
+//         vector<int> left(n,0); // for the previous lesser element
+//         vector<int> right(n,n-1);  // for the next lesser element
+//         // here I have initialized a vector of size n and have initilized every element by n-1
+//         stack<int> s;
+        
+//         int i,height,area,maxArea=0;
+//         // to populate left vector with previous lesser elements
+//         for(i=0;i<n;i++)
+//         {
+//             height=heights[i];
+//             while(!s.empty() && heights[s.top()]>=height)
+//             {
+//                 s.pop();
+//             }
+//             if(!s.empty())
+//             {
+//                 left[i]=s.top()+1;
+//             }
+//             s.push(i);
+//         }
+//         while(!s.empty()) s.pop();
+        
+//         // to populate right vector with next lesser elements
+//         for(i=n-1;i>=0;i--)
+//         {
+//             height=heights[i];
+//             while(!s.empty() && heights[s.top()]>=height)
+//             {
+//                 s.pop();
+//             }
+//             if(!s.empty())
+//             {
+//                 right[i]=s.top()-1;
+//             }
+//             s.push(i);            
+//         }
+        
+//         for(i=0;i<n;i++)
+//         {
+//             area=(right[i]-left[i]+1)*heights[i];
+//             maxArea=max(maxArea,area);
+//         }
+//         return maxArea;
+        
+        
+        int maxArea,height,width;
         int n=heights.size();
-        vector<int> left(n,0); // for the previous lesser element
-        vector<int> right(n,n-1);  // for the next lesser element
-        // here I have initialized a vector of size n and have initilized every element by n-1
         stack<int> s;
         
-        int i,height,area,maxArea=0;
-        // to populate left vector with previous lesser elements
-        for(i=0;i<n;i++)
+        for(int i=0;i<=n;i++)
         {
-            height=heights[i];
-            while(!s.empty() && heights[s.top()]>=height)
+            while(!s.empty() && (i==n || heights[s.top()]>=heights[i]))
             {
+                height=heights[s.top()];
                 s.pop();
-            }
-            if(!s.empty())
-            {
-                left[i]=s.top()+1;
+                if(s.empty()) width=i;
+                else width=i-s.top()-1;
+                maxArea=max(maxArea,width*height);
             }
             s.push(i);
-        }
-        while(!s.empty()) s.pop();
-        
-        // to populate right vector with next lesser elements
-        for(i=n-1;i>=0;i--)
-        {
-            height=heights[i];
-            while(!s.empty() && heights[s.top()]>=height)
-            {
-                s.pop();
-            }
-            if(!s.empty())
-            {
-                right[i]=s.top()-1;
-            }
-            s.push(i);            
-        }
-        
-        for(i=0;i<n;i++)
-        {
-            area=(right[i]-left[i]+1)*heights[i];
-            maxArea=max(maxArea,area);
         }
         return maxArea;
     }
