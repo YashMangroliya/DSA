0b47ed93aac209e9ec9f700aaff88b1ac7cb285e
Yash Mangroliya <77552168+YashMangroliya@users.noreply.github.com>
2022-03-29 13:00:01 +0530

    Time: 610 ms (5.02%), Space: 24.7 MB (24.13%) - LeetHub

--- 503-next-greater-element-ii/503-next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/503-next-greater-element-ii.cpp
@@ -1,36 +1,56 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
-        int greaterNum,i,j,num;
-        bool found=false;
+        // int greaterNum,i,j,num;
+        // bool found=false;
+        // vector<int> ans;
+        // for(i=0;i<nums.size();i++)
+        // {
+        //     num=nums[i];
+        //     found=false;
+        //     for(j=i+1;j<nums.size();j++)
+        //     {
+        //         if(nums[j]>num) 
+        //         {
+        //             greaterNum=nums[j];
+        //             found=true;
+        //             break;
+        //         }
+        //     }
+        //     if(!found)
+        //     {
+        //         for(j=0;j<i;j++)
+        //         {
+        //             if(nums[j]>num) 
+        //             {
+        //                 greaterNum=nums[j];
+        //                 found=true;
+        //                 break;
+        //             }
+        //         }
+        //         if(!found) greaterNum=-1;
+        //     }
+        //     ans.push_back(greaterNum);
+        // }
+        // return ans;
+        
+        
         vector<int> ans;
-        for(i=0;i<nums.size();i++)
+        stack<int> s;
+        int n=nums.size();
+        for(int i=2*n-1;i>=0;i--)
         {
-            num=nums[i];
-            found=false;
-            for(j=i+1;j<nums.size();j++)
+            while(s.empty()==false && s.top()<=nums[i%n])
             {
-                if(nums[j]>num) 
-                {
-                    greaterNum=nums[j];
-                    found=true;
-                    break;
-                }
+                s.pop();
             }
-            if(!found)
+            
+            if(i<n)
             {
-                for(j=0;j<i;j++)
-                {
-                    if(nums[j]>num) 
-                    {
-                        greaterNum=nums[j];
-                        found=true;
-                        break;
-                    }
-                }
-                if(!found) greaterNum=-1;
+                if(s.empty()==false) ans.insert(ans.begin(),s.top());
+                else ans.insert(ans.begin(),-1);
             }
-            ans.push_back(greaterNum);
+            s.push(nums[i%n]);
         }
         return ans;
     }
