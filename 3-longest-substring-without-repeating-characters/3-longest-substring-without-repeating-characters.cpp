71ef400a31e7f9cd3144750949f6f9c7ebc5652f
Yash Mangroliya <77552168+YashMangroliya@users.noreply.github.com>
2022-03-31 13:35:57 +0530

    Time: 12 ms (78.56%), Space: 7.8 MB (80.06%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,26 +1,43 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int length,maxLength=0;
-        string ss;
+        // int length,maxLength=0;
+        // string ss;
+        // char c;
+        // for(int i=0;i<s.length();i++)
+        // {
+        //     ss="";
+        //     length=0;
+        //     for(int j=i;j<s.length();j++)
+        //     {
+        //         c=s.at(j);
+        //         if(ss.find(c,0)!=string::npos)
+        //         {
+        //             break;
+        //         }
+        //         else{
+        //             ss+=c;
+        //             length++;
+        //             maxLength=max(maxLength,length);
+        //         }
+        //     }
+        // }
+        // return maxLength;
+        
+        deque<char> dq;
         char c;
+        int maxLength=0,length;
         for(int i=0;i<s.length();i++)
         {
-            ss="";
-            length=0;
-            for(int j=i;j<s.length();j++)
+            c=s.at(i);
+            if(find(dq.begin(),dq.end(),c)!=dq.end())
             {
-                c=s.at(j);
-                if(ss.find(c,0)!=string::npos)
-                {
-                    break;
-                }
-                else{
-                    ss+=c;
-                    length++;
-                    maxLength=max(maxLength,length);
-                }
+                while(dq.front()!=c) dq.pop_front();
+                dq.pop_front();
             }
+            dq.push_back(c);
+            length=dq.size();
+            maxLength=max(maxLength,length);
         }
         return maxLength;
     }
