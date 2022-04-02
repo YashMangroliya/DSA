7088b980ffc115d9e64942331b6742fdfa286b1e
Yash Mangroliya <77552168+YashMangroliya@users.noreply.github.com>
2022-04-02 14:56:32 +0530

    Time: 99 ms (11.35%), Space: 23.9 MB (39.29%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
     
-    bool checkPallindrome(string s, int index)
+    bool checkPallindrome(string s, int i, int j)
     {
-        string str="";
-        int i,j;
-        for(i=0;i<s.length();i++)
+        while(i<j)
         {
-            if(i!=index)
-            {
-                str+=s[i];
-            }
+            if(s[i]!=s[j]) return false;
+            i++;
+            j--;
         }
-        for(i=0,j=str.length()-1;i<j;i++,j--)
-        {
-            if(str[i]!=str[j]) break;
-        }
-        return i>=j;
+        return true;
     }
     
     bool validPalindrome(string s) {
@@ -26,20 +19,41 @@ public:
         {
             if(s[i]!=s[j])
             {
-                if(flag) return false;
-                if(s[i+1]==s[j] && checkPallindrome(s,i))
-                {
-                    flag=true;
-                    i++;
-                }
-                else if(s[i]==s[j-1] && checkPallindrome(s,j))
-                {
-                    flag=true;
-                    j--;
-                }
-                else return false;
+                return (checkPallindrome(s,i+1,j) || checkPallindrome(s,i,j-1));
             }
         }
         return true;
     }
+
+    
+//     bool checkPallindrome(string s, int index)
+//     {
+//         string str="";
+//         int i,j;
+//         for(i=0;i<s.length();i++)
+//         {
+//             if(i!=index)
+//             {
+//                 str+=s[i];
+//             }
+//         }
+//         for(i=0,j=str.length()-1;i<j;i++,j--)
+//         {
+//             if(str[i]!=str[j]) break;
+//         }
+//         return i>=j;
+//     }
+    
+//     bool validPalindrome(string s) {
+//         bool ans=true;
+//         bool flag=false;
+//         for(int i=0,j=s.length()-1;i<j;i++,j--)
+//         {
+//             if(s[i]!=s[j])
+//             {
+//                 return (checkPallindrome(s,i) || checkPallindrome(s,j));
+//             }
+//         }
+//         return true;
+//     }
 };
