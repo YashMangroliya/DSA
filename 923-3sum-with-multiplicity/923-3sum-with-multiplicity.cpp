84b0323bc10599b31278756daed5f2cda5a925a5
Yash Mangroliya <77552168+YashMangroliya@users.noreply.github.com>
2022-04-07 01:13:13 +0530

    Time: 1701 ms (5.16%), Space: 10.9 MB (20.27%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
-    int threeSumMulti(vector<int>& nums, int target) {
-        if(nums.size()<3) return 0;
-        sort(nums.begin(),nums.end());
-        int required=target,curr,sum;
-        long count=0;        
-        // Method 1 TLE
+    long long mod = 1e9 + 7;
+    
+    int threeSumMulti(vector<int>& arr, int target) {
+        int n = arr.size();
         
-                
-        for(int i=0;i<nums.size()-1;i++)
+        unordered_map<int, int> mp;
+        
+        int count = 0;
+        
+        for(int i = 0; i < n; i++)
         {
-            curr=required-nums[i];
-            unordered_map<int,int> m;
-            for(int j=i+1;j<nums.size();j++)
+            for(int j = i + 1; j < n; j++)
             {
-                count+=m[curr-nums[j]];
-                // if(s.find(curr-nums[j])!=s.end())
-                // {
-                //     count++;
-                // }
-                m[nums[j]]++;
+                int need = target - arr[i] - arr[j];
+                
+                count += mp[need];
             }
+            
+            count %= mod;
+            
+            mp[arr[i]]++;
         }
-        int ans=count%(1000000007);
-        return ans;
-
+        
+        return count % mod;
     }
 };
