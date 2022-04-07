2ea3386c1c1683295594565f891e5d5f0ba2cd26
Yash Mangroliya <77552168+YashMangroliya@users.noreply.github.com>
2022-04-07 11:30:31 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (78.96%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -1,28 +1,53 @@
 class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
-        int first,second,low,high,mid,diff;
-        sort(stones.begin(),stones.end());
+        
+//         Method 1
+        
+//         int first,second,low,high,mid,diff;
+//         sort(stones.begin(),stones.end());
+//         while(stones.size()>1)
+//         {
+//             first=stones.back();
+//             stones.pop_back();
+//             second=stones.back();
+//             stones.pop_back();
+//             if(first!=second)
+//             {
+//                 diff=abs(first-second);
+//                 // logic to find appropriate position for diff in stones vector
+//                 low=0;
+//                 high=stones.size()-1;
+//                 while(low<high)
+//                 {
+//                     mid=(high+low)/2;
+//                     if(stones[mid] <= diff) low=mid+1;
+//                     else high=mid;
+//                 }
+//                 // low is the required position
+//                 stones.insert(stones.begin()+low,diff);
+//             }
+//         }
+//         if(stones.size()==0) return 0;
+//         return stones[0];
+        
+        // Method 2
+        
+        int first,second,diff;
+        make_heap(stones.begin(),stones.end());
         while(stones.size()>1)
         {
-            first=stones.back();
+            first=stones.front();
+            pop_heap(stones.begin(),stones.end());
             stones.pop_back();
-            second=stones.back();
+            second=stones.front();
+            pop_heap(stones.begin(),stones.end());
             stones.pop_back();
-            if(first!=second)
+            diff=abs(first-second);
+            if(diff!=0)
             {
-                diff=abs(first-second);
-                // logic to find appropriate position for diff in stones vector
-                low=0;
-                high=stones.size()-1;
-                while(low<high)
-                {
-                    mid=(high+low)/2;
-                    if(stones[mid] <= diff) low=mid+1;
-                    else high=mid;
-                }
-                // low is the required position
-                stones.insert(stones.begin()+low,diff);
+                stones.push_back(diff);
+                push_heap(stones.begin(),stones.end());
             }
         }
         if(stones.size()==0) return 0;
