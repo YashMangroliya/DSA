48ae8c576fafa7f27250c5d5837612b5ab473460
Yash Mangroliya <77552168+YashMangroliya@users.noreply.github.com>
2022-04-09 11:41:34 +0530

    Time: 32 ms (17.99%), Space: 13.7 MB (71.77%) - LeetHub

---
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -0,0 +1,118 @@
+class Solution {
+public:
+    
+    vector<int> heap;
+    map<int,int> m;
+    
+//     void add(int num)
+//     {
+//         int ri,ci,swi,g,lci,rci;
+//         ci=heap.size();
+//         heap.push_back(num);
+//         while(ci>0)
+//         {
+//             ri=(ci-1)/2;
+//             if(m[heap[ci]] < m[heap[ri]])
+//             {
+//                 g=heap[ci];
+//                 heap[ci]=heap[ri];
+//                 heap[ri]=g;
+                
+//                 ci=ri;
+//             }
+//             else break;
+//         }
+        
+//         if(heap.size()>size)
+//         {
+//             swap(heap[0],heap[heap.size()-1]);
+//             heap.pop_back();
+//             ri=0;
+//             while(1)
+//             {
+//                 lci=ri*2+1;
+//                 rci=lci+1;
+//                 if(lci>=this->size) break;
+//                 if(rci>=this->size) swi=lci;
+//                 else
+//                 {
+//                     if(m[heap[lci]] < m[heap[rci]] ) swi=lci;
+//                     else swi=rci;
+//                 }
+                
+//                 if(m[heap[swi]] < m[heap[ri]])
+//                 {
+//                     swap(heap[ri],heap[swi]);
+//                     ri=swi;    
+//                 }
+//                 else break;
+//             }
+//         }
+//     }
+    
+    vector<int> getTopK(int k)
+    {
+        vector<int> ans;
+        int ri,ci,swi,g,lci,rci;
+        while(k>0)
+        {
+            ans.push_back(heap[0]);
+            swap(heap[0],heap[heap.size()-1]);
+            heap.pop_back();
+            ri=0;
+            while(1)
+            {
+                lci=ri*2+1;
+                rci=lci+1;
+                if(lci>=this->heap.size()) break;
+                if(rci>=this->heap.size()) swi=lci;
+                else
+                {
+                    if(m[heap[lci]] > m[heap[rci]] ) swi=lci;
+                    else swi=rci;
+                }
+
+                if(m[heap[swi]] > m[heap[ri]])
+                {
+                    swap(heap[ri],heap[swi]);
+                    ri=swi;    
+                }
+                else break;
+            }
+            k--;
+        }
+        return ans;
+    }
+    
+    void createHeap()
+    {
+        int ri,ci,g;
+        for(int i=0;i<heap.size();i++)
+        {
+            ci=i;
+            while(ci>0)
+            {
+                ri=(ci-1)/2;
+                if(m[heap[ci]] > m[heap[ri]])
+                {
+                    g=heap[ci];
+                    heap[ci]=heap[ri];
+                    heap[ri]=g;
+
+                    ci=ri;
+                }
+                else break;
+            }
+        }
+    }
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        for(int num : nums)
+        {
+            m[num]++;
+        }
+        for(auto entry : m) heap.push_back(entry.first);
+        createHeap();
+        vector<int> ans = getTopK(k);
+        return ans;
+    }
+};
