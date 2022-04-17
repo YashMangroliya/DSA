240b0cd1986e5fcac1a17aeb083eba54b8e748fc
Yash Mangroliya <77552168+YashMangroliya@users.noreply.github.com>
2022-04-18 00:23:12 +0530

    Time: 0 ms (100.00%), Space: 8 MB (45.48%) - LeetHub

--- 897-increasing-order-search-tree/897-increasing-order-search-tree.cpp
+++ 897-increasing-order-search-tree/897-increasing-order-search-tree.cpp
@@ -11,22 +11,25 @@
  */
 class Solution {
 public:
-    TreeNode* ans = NULL, *tmp = NULL;
-    void fun_inorder(TreeNode* root){
-        if(!root)return;
-        fun_inorder(root->left);
-        if(!ans){
+    TreeNode * ans=NULL, * tmp=NULL;
+    void inorder(TreeNode * root)
+    {
+        if(root==NULL) return;
+        inorder(root->left);
+        if(!ans)
+        {
             ans = new TreeNode(root->val);
             tmp = ans;
         }
-        else{
+        else
+        {
             tmp->right = new TreeNode(root->val);
             tmp = tmp->right;
         }
-        fun_inorder(root->right);
+        inorder(root->right);
     }
     TreeNode* increasingBST(TreeNode* root) {
-         fun_inorder(root);
-         return ans;
+        inorder(root);
+        return ans;
     }
 };
