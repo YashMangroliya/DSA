2a0f5afb52d38e982023455b19dbf579bf58ef47
Yash Mangroliya <77552168+YashMangroliya@users.noreply.github.com>
2022-04-22 23:01:17 +0530

    Time: 109 ms (14.19%), Space: 7.6 MB (67.96%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
+//     Method 1
+    
     int dp[502][502];
     int rec(string& s, string& t, int i, int j)
     {
-        if(j>=t.length()) {
-            return s.length()-i;
+        if(j<0) {
+            return i+1;
         }
-        if(i>=s.length()) {
-            return t.length()-j;
+        if(i<0) {
+            return j+1;
         }
         if(dp[i][j]!=-1) return dp[i][j];
         int ans;
         if(s[i]==t[j])
         {
-            ans = rec(s,t,i+1,j+1);
+            ans = rec(s,t,i-1,j-1);
         }
         else
         {
             int count1,count2,count3;
-            count1 = 1 + rec(s,t,i,j+1);  // insert
+            count1 = 1 + rec(s,t,i,j-1);  // insert
             
-            count2 = 1 + rec(s,t,i+1,j);  // remove
+            count2 = 1 + rec(s,t,i-1,j);  // remove
             
-            count3 = 1 + rec(s,t,i+1,j+1);  // replace
+            count3 = 1 + rec(s,t,i-1,j-1);  // replace
             ans = min({count1,count2,count3});
         }
         dp[i][j]=ans;
@@ -31,6 +33,12 @@ public:
 
     int minDistance(string word1, string word2) {
         memset(dp,-1,sizeof(dp));
-        return rec(word1,word2,0,0);
+        return rec(word1,word2,word1.length()-1,word2.length()-1);
     }
+    
+    
+    // Method 2
+//     int minDistance(string s, string t) {
+        
+//     }
 };
