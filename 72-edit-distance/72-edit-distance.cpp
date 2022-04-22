fc04ea15ef020b806483f94182029be77fc5f00c
Yash Mangroliya <77552168+YashMangroliya@users.noreply.github.com>
2022-04-22 20:14:28 +0530

    Time: 238 ms (6.57%), Space: 79.8 MB (11.96%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,41 +1,36 @@
 class Solution {
 public:
-    map<string,int> m;
-    int rec(string s, string t, int i)
+    int dp[502][502];
+    int rec(string s, string t, int i, int j)
     {
-        if(i>=t.length() || i>=s.length()) {
-            int ans=t.length()-s.length();
-            if(ans>=0) return ans;
-            else return ans*-1;
+        if(j>=t.length()) {
+            return s.length()-i;
         }
-        if(m.find(s)!=m.end()) return m[s];
+        if(i>=s.length()) {
+            return t.length()-j;
+        }
+        if(dp[i][j]!=-1) return dp[i][j];
         int ans;
-        if(s[i]==t[i])
+        if(s[i]==t[j])
         {
-            ans = rec(s,t,i+1);
+            ans = rec(s,t,i+1,j+1);
         }
         else
         {
-            string ss;
             int count1,count2,count3;
-            ss=s;
-            ss.insert(ss.begin()+i,t[i]);
-            count1 = rec(ss,t,i+1);
+            count1 = 1 + rec(s,t,i,j+1);  // insert
             
-            ss=s;
-            ss.erase(ss.begin()+i);
-            count2 = rec(ss,t,i);
+            count2 = 1 + rec(s,t,i+1,j);  // remove
             
-            ss=s;
-            ss[i]=t[i];
-            count3 = rec(ss,t,i+1);
-            ans = min({count1,count2,count3})+1;
+            count3 = 1 + rec(s,t,i+1,j+1);  // replace
+            ans = min({count1,count2,count3});
         }
-        m[s]=ans;
+        dp[i][j]=ans;
         return ans;
     }
 
     int minDistance(string word1, string word2) {
-        return rec(word1,word2,0);
+        memset(dp,-1,sizeof(dp));
+        return rec(word1,word2,0,0);
     }
 };
