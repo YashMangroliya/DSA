5633b565416fe55da97155101ea042c1224ddfaf
Yash Mangroliya <77552168+YashMangroliya@users.noreply.github.com>
2022-06-12 16:30:49 +0530

    Time: 567 ms (39.25%), Space: 127.2 MB (35.12%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -1,26 +1,45 @@
 class Solution {
 public:
     int maximumUniqueSubarray(vector<int>& nums) {
-        deque<int> dq;
-        int sum=0,gsum=0;
-        for(int i : nums)
+//         TLE 
+    //     deque<int> dq;
+    //     int sum=0,gsum=0;
+    //     for(int i : nums)
+    //     {
+    //         if(find(dq.begin(),dq.end(),i)!=dq.end())
+    //         {
+    //             while(dq.front()!=i)
+    //             {
+    //                 sum-=dq.front();
+    //                 dq.pop_front();
+    //             }
+    //             dq.pop_front();
+    //             dq.push_back(i);
+    //         }
+    //         else
+    //         {
+    //             dq.push_back(i);
+    //             sum+=i;
+    //             gsum=max(gsum,sum);
+    //         }
+    //     }
+    //     return gsum;
+    // }
+        
+        unordered_set<int> set1;
+        int sum=0,gsum=0,i=0;
+        for(int num : nums)
         {
-            if(find(dq.begin(),dq.end(),i)!=dq.end())
+            while(set1.count(num)>0)
             {
-                while(dq.front()!=i)
-                {
-                    sum-=dq.front();
-                    dq.pop_front();
-                }
-                dq.pop_front();
-                dq.push_back(i);
+                set1.erase(nums[i]);
+                sum-=nums[i];
+                i++;
             }
-            else
-            {
-                dq.push_back(i);
-                sum+=i;
-                gsum=max(gsum,sum);
-            }
+            set1.insert(num);
+            sum+=num;
+           
+            gsum=max(gsum,sum);
         }
         return gsum;
     }
