c1e43bd51dc9ab6ea6b6708dc38975e473a89c7d
Yash Mangroliya <77552168+YashMangroliya@users.noreply.github.com>
2022-06-12 16:12:29 +0530

    Time: 1612 ms (5.11%), Space: 94.2 MB (92.59%) - LeetHub

---
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maximumUniqueSubarray(vector<int>& nums) {
+        deque<int> dq;
+        int sum=0,gsum=0;
+        for(int i : nums)
+        {
+            if(find(dq.begin(),dq.end(),i)!=dq.end())
+            {
+                while(dq.front()!=i)
+                {
+                    sum-=dq.front();
+                    dq.pop_front();
+                }
+                dq.pop_front();
+                dq.push_back(i);
+            }
+            else
+            {
+                dq.push_back(i);
+                sum+=i;
+                gsum=max(gsum,sum);
+            }
+        }
+        return gsum;
+    }
+};
