5ee07506fada8d764575f8c25fab55a3a5889ad8
Yash Mangroliya <77552168+YashMangroliya@users.noreply.github.com>
2022-06-16 00:18:29 +0530

    Time: 757 ms (6.41%), Space: 13.4 MB (71.28%) - LeetHub

---
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    
+    int dp[1000];
+    int solve(vector<string> &words,int index)
+    {
+        if(dp[index]!=-1) return dp[index];
+        int ans=0;
+        string word=words[index];
+        vector<string>::iterator itr;
+        string newWord;
+        for(int i=0;i<word.length();i++)
+        {
+            newWord=word.substr(0,i)+word.substr(i+1);
+            itr=find(words.begin()+index,words.end(),newWord);
+            if(itr!=words.end())
+            {
+                ans=max(ans, solve(words,itr-words.begin()));
+            }
+        }
+        dp[index]=ans+1;;
+        return ans+1;
+    }
+    
+    int longestStrChain(vector<string>& words) {
+        sort(words.begin(),words.end(), [] (const string &s1, const string &s2)         { 
+            return s1.length()>s2.length();
+        });
+        
+        memset(dp,-1,sizeof(dp));
+        //for(string i: words) cout<<i<<end;
+        int ans=1;
+        for(int i=0;i<words.size();i++)
+        {
+            ans=max(ans,solve(words,i));
+        }
+        return ans;
+    }
+};
