fe7c43046547e91d7f306afae1d9a4e5bd652a32
Yash Mangroliya <77552168+YashMangroliya@users.noreply.github.com>
2022-06-16 00:54:59 +0530

    Time: 206 ms (27.52%), Space: 51.4 MB (10.23%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,39 +1,64 @@
 class Solution {
 public:
     
-    int dp[1000];
-    int solve(vector<string> &words,int index)
-    {
-        if(dp[index]!=-1) return dp[index];
-        int ans=0;
-        string word=words[index];
-        vector<string>::iterator itr;
-        string newWord;
-        for(int i=0;i<word.length();i++)
-        {
-            newWord=word.substr(0,i)+word.substr(i+1);
-            itr=find(words.begin()+index,words.end(),newWord);
-            if(itr!=words.end())
-            {
-                ans=max(ans, solve(words,itr-words.begin()));
-            }
-        }
-        dp[index]=ans+1;;
-        return ans+1;
-    }
+    // Less Optimized
+    
+//     int dp[1000];
+//     int solve(vector<string> &words,int index)
+//     {
+//         if(dp[index]!=-1) return dp[index];
+//         int ans=0;
+//         string word=words[index];
+//         vector<string>::iterator itr;
+//         string newWord;
+//         for(int i=0;i<word.length();i++)
+//         {
+//             newWord=word.substr(0,i)+word.substr(i+1);
+//             itr=find(words.begin()+index,words.end(),newWord);
+//             if(itr!=words.end())
+//             {
+//                 ans=max(ans, solve(words,itr-words.begin()));
+//             }
+//         }
+//         dp[index]=ans+1;;
+//         return ans+1;
+//     }
+    
+//     int longestStrChain(vector<string>& words) {
+//         sort(words.begin(),words.end(), [] (const string &s1, const string &s2)         { 
+//             return s1.length()>s2.length();
+//         });
+        
+//         memset(dp,-1,sizeof(dp));
+//         //for(string i: words) cout<<i<<end;
+//         int ans=1;
+//         for(int i=0;i<words.size();i++)
+//         {
+//             ans=max(ans,solve(words,i));
+//         }
+//         return ans;
+//     }
+    
     
     int longestStrChain(vector<string>& words) {
-        sort(words.begin(),words.end(), [] (const string &s1, const string &s2)         { 
-            return s1.length()>s2.length();
-        });
+         sort(words.begin(),words.end(), [] (const string &s1, const string &s2)                { 
+              return s1.length()<s2.length();
+         });
         
-        memset(dp,-1,sizeof(dp));
-        //for(string i: words) cout<<i<<end;
-        int ans=1;
-        for(int i=0;i<words.size();i++)
+        int ans=0;
+        string newWord;
+        unordered_map<string,int> map1;
+        for(string word : words)
         {
-            ans=max(ans,solve(words,i));
+            int longest=0;
+            for(int i=0;i<word.length();i++)
+            {
+                newWord=word.substr(0,i) + word.substr(i+1,word.length()+1);
+                longest=max(longest,map1[newWord]+1);
+            }
+            map1[word]=longest;
+            ans=max(ans,longest);
         }
         return ans;
-    }
+     }
 };
