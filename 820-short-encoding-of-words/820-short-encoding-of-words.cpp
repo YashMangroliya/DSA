cea82cedb830bc785ba2d8b6d3a2eeb69380eeaa
Yash Mangroliya <77552168+YashMangroliya@users.noreply.github.com>
2022-06-21 01:10:49 +0530

    Time: 104 ms (58.33%), Space: 37.1 MB (50.49%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.cpp
+++ 820-short-encoding-of-words/820-short-encoding-of-words.cpp
@@ -5,13 +5,13 @@ public:
         return first.size() > second.size();
     }
     int minimumLengthEncoding(vector<string>& words) {
-//         sort(words.begin(),words.end(),[](string e1,string e2){ return e1.length()<e2.length(); });
+//         sort(words.begin(),words.end(),[](string &e1,string &e2){ return e1.length()<e2.length(); });
 //         string s="";
 //         bool flag=false;
 //         for(int i=words.size()-1;i>=0;i--)
 //         {
 //             flag=false;
-//             string word=words[i];
+//             string word=words[i]+"#";
 //             int index=s.find(word);
 //             // cout<<"Index value: "<<index<<endl;
 //             // cout<<s<<endl;
