e5610c49b52a6518da1961e18617ad9850721d44
Yash Mangroliya <77552168+YashMangroliya@users.noreply.github.com>
2022-06-23 01:41:39 +0530

    Time: 126 ms (83.52%), Space: 53.7 MB (76.13%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
@@ -4,11 +4,11 @@ public:
     int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
         
         // Giving wrong answer in some test cases
-//         vector<int> maximums;
-//         int prev=heights[0];
-//         int height, lowestAmongMaximums, i, diff;
-//         long long int sum;
-//         int s=ladders;
+        vector<int> maximums;
+        int prev=heights[0];
+        int height, lowestAmongMaximums, i, diff;
+        long long int sum;
+        int s=ladders;
         
         struct greaters{ 
             bool operator()(const long& a,const long& b) const{ 
@@ -16,78 +16,79 @@ public:
             } 
         }; 
         
-//         if(ladders==0)
-//         {
-//             sum=0;
-//             for(i=1;i<heights.size() && sum<=bricks; i++)
-//             {
-//                 if(heights[i]>prev)
-//                 {
-//                     sum+=(heights[i]-prev);
-//                     if(sum>bricks) break;
-//                 }
-//                 prev=heights[i];
-//             }
-//             return i-1;
-//         }
+        if(ladders==0)
+        {
+            sum=0;
+            for(i=1;i<heights.size() && sum<=bricks; i++)
+            {
+                if(heights[i]>prev)
+                {
+                    sum+=(heights[i]-prev);
+                    if(sum>bricks) break;
+                }
+                prev=heights[i];
+            }
+            return i-1;
+        }
         
-//         for(i=1;i<heights.size();i++)
-//         {
-//             height=heights[i];
-//             if(height>prev)
-//             {
-//                 diff=height-prev;
-//                 if(ladders>0)
-//                 {
-//                     maximums.push_back( diff );
-//                     ladders--;
-//                     if(ladders==0) make_heap(maximums.begin(),maximums.end(),greater());
-//                 }
-//                 else
-//                 {
-//                     if(diff <= maximums.front())
-//                     {
-//                         if(diff > bricks) break;
-//                         else bricks-=diff;
-//                     }
-//                     else
-//                     {
-//                         lowestAmongMaximums=maximums.front();
-//                         pop_heap(maximums.begin(),maximums.end(),greater());
-//                         maximums.push_back(diff);
-//                         push_heap(maximums.begin(),maximums.end(),greater());
-//                         if(lowestAmongMaximums > bricks) break;
-//                         else bricks -= lowestAmongMaximums;
-//                     }
-//                 }
-//             }
-//             prev=height;
-//         }
-//         return i-1;
+        for(i=1;i<heights.size();i++)
+        {
+            height=heights[i];
+            if(height>prev)
+            {
+                diff=height-prev;
+                if(ladders>0)
+                {
+                    maximums.push_back( diff );
+                    ladders--;
+                    if(ladders==0) make_heap(maximums.begin(),maximums.end(),greater());
+                }
+                else
+                {
+                    if(diff <= maximums.front())
+                    {
+                        if(diff > bricks) break;
+                        else bricks-=diff;
+                    }
+                    else
+                    {
+                        lowestAmongMaximums=maximums.front();
+                        pop_heap(maximums.begin(),maximums.end(),greater());
+                        maximums.pop_back();
+                        maximums.push_back(diff);
+                        push_heap(maximums.begin(),maximums.end(),greater());
+                        if(lowestAmongMaximums > bricks) break;
+                        else bricks -= lowestAmongMaximums;
+                    }
+                }
+            }
+            prev=height;
+        }
+        return i-1;
         
         // Solution
         
         
-        priority_queue<ll, vector<ll> , greater<ll>> pq;
+//         priority_queue<ll, vector<ll> , greater<ll>> pq;
         
-        ll n=heights.size();
-        ll i,diff;
-        for(i=0;i<n-1;i++)
-        {
-            diff=heights[i+1]-heights[i];
-            if(diff>0)
-            {
-                pq.push(diff);
-                if(pq.size()<=ladders) continue;
-                if(pq.top()>bricks) return i;
-                else 
-                {
-                    bricks -= pq.top();
-                    pq.pop();
-                }
-            }
-        }
-        return n-1;
+//         ll n=heights.size();
+//         ll i,diff;
+//         for(i=0;i<n-1;i++)
+//         {
+//             diff=heights[i+1]-heights[i];
+//             if(diff>0)
+//             {
+//                 pq.push(diff);
+//                 if(pq.size()<=ladders) continue;
+//                 if(pq.top()>bricks) return i;
+//                 else 
+//                 {
+//                     bricks -= pq.top();
+//                     pq.pop();
+//                 }
+//             }
+//         }
+        //return n-1;
         
     }
 };
