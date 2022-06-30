14447b11864232f0bbbe001d7ae3ee2286b29430
Yash Mangroliya <77552168+YashMangroliya@users.noreply.github.com>
2022-07-01 01:12:01 +0530

    Time: 24 ms (17.15%), Space: 10.7 MB (99.77%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -2,15 +2,26 @@ class Solution {
 public:
     int minMoves2(vector<int>& nums) {
         
-        int median,n,sum;
+        // Approach 3
         sort(nums.begin(),nums.end());
-        n=nums.size();
-        if(nums.size()%2==0) median = (nums[n/2]+nums[n/2-1])/2;
-        else median=nums[n/2];
-        
-        sum=0;
-        for(int num:nums) sum+=abs(num-median);
+        int sum=0;
+        for(int i=0,j=nums.size()-1;i<j;i++,j--)
+        {
+            sum+= (nums[j]-nums[i]);
+        }
         return sum;
+        
+        
+        // Approach 2 
+//         int median,n,sum;
+//         sort(nums.begin(),nums.end());
+//         n=nums.size();
+//         if(nums.size()%2==0) median = (nums[n/2]+nums[n/2-1])/2;
+//         else median=nums[n/2];
+        
+//         sum=0;
+//         for(int num:nums) sum+=abs(num-median);
+//         return sum;
         
         
         // Approach 1: Gives TLE
