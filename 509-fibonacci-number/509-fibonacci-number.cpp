2f65e3b2fa573a1b3f995ed0e9e3d0696a2786bb
Yash Mangroliya <77552168+YashMangroliya@users.noreply.github.com>
2022-07-06 22:11:18 +0530

    Time: 3 ms (60.76%), Space: 5.9 MB (41.61%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int t[31] = {0}; // used for to store results
+    int fib(int n) {
+        if(n == 0 ||  n == 1) // it is base condition, if n is zero or one
+            return n; // then return 1
+        
+        if(t[n] != 0) // if we already precompute it, no need to compute again 
+            return t[n];
+        
+        
+        return t[n] = fib(n - 1) + fib(n - 2); // return what it wants
+        
+    }
+};
