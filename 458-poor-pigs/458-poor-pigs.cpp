ec1d05a5dcf6a6f9bf363d43d1d40e59a406e7b9
Yash Mangroliya <77552168+YashMangroliya@users.noreply.github.com>
2022-08-06 23:52:29 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (22.55%) - LeetHub

--- 458-poor-pigs/458-poor-pigs.cpp
+++ 458-poor-pigs/458-poor-pigs.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
-    int poorPigs(int buck, int Die, int Test) {
-        int y = (Test/Die) + 1;
-        int z = y;
-        int count = 1;
-        if(buck==1){
-            return 0;
-        }
+    int poorPigs(int buckets, int poisonTime, int totalTime) {
+//         int y = (Test/Die) + 1;
+//         int z = y;
+//         int count = 1;
+//         if(buck==1){
+//             return 0;
+//         }
 
-        while(y<buck){
+//         while(y<buck){
 
-            y = y * z;
-            count++;
+//             y = y * z;
+//             count++;
 
-        }
-        return count;
+//         }
+//         return count;
+        
+        return ceil(log(buckets) / log(totalTime / poisonTime + 1));
     }
 };
