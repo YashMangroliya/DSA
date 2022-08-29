2bb75c9008824f2713c6c1ab9e99be2e4de96622
Yash Mangroliya <77552168+YashMangroliya@users.noreply.github.com>
2022-08-30 00:29:17 +0530

    Time: 58 ms (47.98%), Space: 13.3 MB (30.43%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -5,17 +5,7 @@ public:
         if(i<0 || i>=grid.size() || j<0 || j>=grid[0].size() || grid[i][j]=='0' || visited[i][j]==1) return;
         int row=i,col=j;
         visited[row][col]=1;
-        // vector<int> deltaI={-1,0,+1,0};
-        // vector<int> deltaJ={0,+1,0,-1};
-//         for(int k=0;k<4;k++)
-//         {
-//             row=i+deltaI[k];
-//             col=j+deltaJ[k];
-//             // if(row<0 || row>=grid.size() || col<0 || col>=grid[0].size() || visited[row][col]==1 || grid[row][col]=='0') continue;
-            
-//             // if(row>=0 && row<grid.size() && col>=0 && col<grid[0].size() && visited[row][col]!=1 && grid[row][col]!='0') dfs(row,col,grid,visited);
-//             dfs(row,col,grid,visited);
-//         }
+        
         dfs(row-1,col,grid,visited);
         dfs(row,col+1,grid,visited);
         dfs(row+1,col,grid,visited);
