db78efe048e747e87dce1d066b28de1dedb8eb3f
Yash Mangroliya <77552168+YashMangroliya@users.noreply.github.com>
2022-09-04 14:51:21 +0530

    Time: 6 ms (74.10%), Space: 12.2 MB (84.68%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -9,10 +9,29 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+/*
+LOGIC:
+
+Step 1:
+I will traverse over the whole tree and will add all the nodes in a vector.
+This vector will be a vector of pairs, first value of this pair will be nodeValue and second value of this pair will be a pair of row and column.
+
+Step 2:
+After this I will make a new vector , will traverse on the previous vector and add value to this vector by taking the column value as index value.
+
+
+Step 3:
+will sort this just previous vector on the basis of row number, if row number if same then on the basis of nodeValue
+
+Step 4:
+After this will make another vector, will traverse the just previous vector and add to this vector(in previous vector we were adding pairs so that we can sort on basis of row, now so that it has been sorted we will omitt the column value and will only add the nodeValue to vector which is required as answer)
+*/
+
 class Solution {
 public:
     int upperBoundColumn=INT_MIN;
     int lowerBoundColumn=INT_MAX;
+    
     void preorder(TreeNode* node,vector<pair<int,pair<int,int>>>& vec,int row,int col)
     {
         if(node==nullptr) return;
@@ -22,17 +41,20 @@ public:
         preorder(node->left,vec,row+1,col-1);
         preorder(node->right,vec,row+1,col+1);
     }
+    
     vector<vector<int>> verticalTraversal(TreeNode* root) {
         vector<pair<int,pair<int,int>>> vec;
-        preorder(root,vec,0,0);
+        preorder(root,vec,0,0); // Step 1
         int numberOfColumns=upperBoundColumn-lowerBoundColumn+1;
         vector<vector<pair<int,int>>> ans(numberOfColumns,vector<pair<int,int>>());
         int nodeValue;
+        // Step 2
         for(pair<int,pair<int,int>>& p : vec)
         {
             nodeValue=p.first;
             ans[p.second.second-lowerBoundColumn].push_back(make_pair(nodeValue,p.second.first));
         }
+        // Step 3
         for(int i=0;i<ans.size();i++)
         {
             sort(ans[i].begin(),ans[i].end(),[](const pair<int,int>& p1,const pair<int,int>& p2){
@@ -40,6 +62,7 @@ public:
                 return p1.first<p2.first;
             });
         }
+        // Step 4
         vector<vector<int>> ans1;
         for(vector<pair<int,int>>& pv : ans)
         {
