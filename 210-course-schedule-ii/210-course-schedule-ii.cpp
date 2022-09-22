fd22bf62c919f52c55cfc93dba3b5dfd89366ad0
Yash Mangroliya <77552168+YashMangroliya@users.noreply.github.com>
2022-09-23 00:53:37 +0530

    Time: 52 ms (21.14%), Space: 13.2 MB (90.48%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adjecencyList(numCourses,vector<int>());
+        for(int i=0;i<prerequisites.size();i++)
+        {
+            adjecencyList[prerequisites[i][1]].push_back(prerequisites[i][0]);
+        }
+        int V=numCourses;
+        vector<int> indegree(V,0);
+	    queue<int> que;
+	    vector<int> ans;
+	    int node;
+	    for(int i=0;i<V;i++)
+	    {
+	        for(int index : adjecencyList[i])
+	        {
+	            indegree[index]++;
+	        }
+	    }
+	    for(int i=0;i<V;i++) 
+	    {
+	        if(indegree[i]==0) que.push(i);
+	    }
+	    while(!que.empty())
+	    {
+	        node=que.front();
+	        que.pop();
+	        ans.push_back(node);
+	        for(int index : adjecencyList[node])
+	        {
+	            indegree[index]--;
+	            if(indegree[index]==0) que.push(index);
+	        }
+	    }
+	    if(ans.size()==V) return ans;
+        return vector<int>();
+    }
+};
