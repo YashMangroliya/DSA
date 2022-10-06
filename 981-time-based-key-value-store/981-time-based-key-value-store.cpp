b9de44663cca404fa210425a7481d05696f6b3d3
Yash Mangroliya <77552168+YashMangroliya@users.noreply.github.com>
2022-10-07 00:59:33 +0530

    Time: 1604 ms (5.00%), Space: 146.3 MB (5.03%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.cpp
+++ 981-time-based-key-value-store/981-time-based-key-value-store.cpp
@@ -2,7 +2,6 @@ class TimeMap {
 private:
     map<pair<string,int>,string> map1;
     vector<int> timestamps;
-    bool sorted=true;
 public:
     TimeMap() {
         
@@ -11,19 +10,14 @@ public:
     void set(string key, string value, int timestamp) {
         pair<string,int> p=make_pair(key,timestamp);
         map1[p]=value;
-        // int index = upper_bound(timestamps.begin(),timestamps.end(),timestamp)-timestamps.begin();
-        timestamps.insert(upper_bound(timestamps.begin(),timestamps.end(),timestamp),timestamp);
-        sorted=false;
+        timestamps.insert(
+             upper_bound(timestamps.begin(),timestamps.end(),timestamp),timestamp);
     }
     
     string get(string key, int timestamp) {
         if(map1.find(make_pair(key,timestamp))!=map1.end()) return map1[make_pair(key,timestamp)];
-        // if(!sorted){
-        //     sort(timestamps.begin(),timestamps.end());
-        //     sorted=true;
-        // }
+        
         int index=upper_bound(timestamps.begin(),timestamps.end(),timestamp)-timestamps.begin()-1;
-        //cout<<"key: "<<key<<", timestamp"<<timestamp<<", index:"<<index<<endl;
         while(index>=0)
         {
             if(map1.find(make_pair(key,timestamps[index]))!=map1.end())
