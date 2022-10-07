b7376028cb0f60a9c57fb861b35912c60c5303e3
Yash Mangroliya <77552168+YashMangroliya@users.noreply.github.com>
2022-10-08 00:49:46 +0530

    Time: 283 ms (24.40%), Space: 24.5 MB (98.51%) - LeetHub

---
+++ 732-my-calendar-iii/732-my-calendar-iii.cpp
@@ -0,0 +1,45 @@
+class MyCalendarThree {
+public:
+    vector<pair<int, int>>all;
+    int n;
+    
+    MyCalendarThree() {
+        n = 0;
+    }
+    
+    int book(int start, int end) {
+        // inserting the start point in the sorted array
+        pair<int, int>val = {start, 0}; // 0 indicates arrival
+        auto pos = lower_bound(all.begin(), all.end(), val);
+        all.insert(pos, val);
+        
+        // inserting the second point in the sorted array
+        val = {end-1, 1}; // 1 indicates departure
+        pos = lower_bound(all.begin(), all.end(), val);
+        all.insert(pos, val);
+        
+        n += 2; // since added two points
+        
+        int maxi = 0;
+        
+        // finding the maximum intersection of partitions
+        int res = 0;
+        for(int i=0; i<n; i++){
+            auto t = all[i];
+            if(t.second == 0)
+                res++;
+            else
+                res--;
+            
+            maxi = max(maxi, res);
+        }
+        
+        return maxi;
+    }
+};
+
+/**
+ * Your MyCalendarThree object will be instantiated and called as such:
+ * MyCalendarThree* obj = new MyCalendarThree();
+ * int param_1 = obj->book(start,end);
+ */
