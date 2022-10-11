fedfb0361e44152fa59491a1ac0653543d7455bb
Yash Mangroliya <77552168+YashMangroliya@users.noreply.github.com>
2022-10-11 23:24:31 +0530

    Time: 157 ms (28.08%), Space: 65.9 MB (8.51%) - LeetHub

---
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int n=nums.size();
+        int lesser[n];
+        int greater[n];
+        int mini,maxi;
+        mini=0;
+        lesser[0]=-1;
+        for(int i=0;i<n-1;i++)
+        {
+            if(nums[mini]<nums[i])
+            {
+                lesser[i]=mini;
+            }
+            else
+            {
+                mini=i;
+                lesser[i]=-1;
+            }
+        }
+        maxi=n-1;
+        greater[n-1]=-1;
+        for(int i=n-1;i>=0;i--)
+        {
+            if(nums[i]<nums[maxi])
+            {
+                greater[i]=maxi;
+            }
+            else
+            {
+                maxi=i;
+                greater[i]=-1;
+            }
+        }
+        
+        for(int i=0;i<n;i++)
+        {
+            if(lesser[i]!=-1 && greater[i]!=-1) return true;
+        }
+        return false;
+    }
+};
