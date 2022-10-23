2347014bdeadff9eba08ac57d8f4bd9613f1c274
Yash Mangroliya <77552168+YashMangroliya@users.noreply.github.com>
2022-10-24 01:04:04 +0530

    Time: 167 ms (14.42%), Space: 31 MB (21.22%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        // sort(nums.begin(),nums.end());
+        unordered_set<int> set1;
+        vector<int> ans(2);
+        for(int i=0;i<nums.size();i++)
+        {
+            if(set1.count(nums[i])>0) ans[0]=nums[i];
+            else set1.insert(nums[i]);
+        }
+        for(int i=1;i<=nums.size();i++)
+        {
+            if(set1.count(i)==0)
+            {
+                ans[1]=i;
+                return ans;
+            }
+        }
+        return ans;
+    }
+};
